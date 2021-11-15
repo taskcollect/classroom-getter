b689451fe1d06d41b074cde55863676d683a964d
anthello147 <61529100+anthello147@users.noreply.github.com>
2021-11-15 13:48:47 +0000

    What I've done so far

--- src/main.go
+++ src/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"main/gc"
 	"net/http"
+
+	"github.com/buger/jsonparser"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	req, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("400: Failed to read request body.")
+	}
+
+	secret, _, _, err := jsonparser.Get(req, "secret")
+	if err != nil {
+		log.Fatalf("400: Failed to get secret from JSON request body. %v", err)
+	}
+
+	tasks := gc.GetTasks(secret)
+	w.Write(tasks)
 }
 
 func main() {
